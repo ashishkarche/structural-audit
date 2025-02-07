f43f4a188856d669ad2eef7483185f57f51ff455
ashishkarche <ashishkarche9@gmail.com>
2025-02-07 17:39:13 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -11,14 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const SECRET_KEY = process.env.JWT_SECRET;
 
-const corsOptions = {
-  origin: ['https://structural-audit-6xw4.vercel.app'], // Allow requests only from your frontend URL
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  allowedHeaders: 'Content-Type, Authorization',
-};
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Handle preflight requests
+app.use(cors());
 app.use(express.json());
 
 // Database connection (using Pool for better efficiency)
@@ -51,6 +44,10 @@ const authenticate = async (req, res, next) => {
     res.status(400).json({ message: 'Invalid Token' });
   }
 };
+
+app.get("/",(req,res) =>{
+  res.json("Sucess");
+})
 
 // Register auditor
 app.post('/register', async (req, res) => {
